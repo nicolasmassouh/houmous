36ec04998ff9c6820b8f4dee80cb372b394d9db3
nico <massouhnico@gmail.com>
2016-07-18 17:17:32 +0200

    add configurations project

---
+++ app/cats.js
@@ -0,0 +1,5 @@
+/**
+ * Created by nico on 06/07/2016.
+ */
+var aCats = ['dave', 'henry', 'martha'];
+module.exports = aCats;
---
+++ webpack.config.js
@@ -0,0 +1,19 @@
+/**
+ * Created by nico on 06/07/2016.
+ */
+module.exports = {
+    entry: './app/app.js',
+    output: {
+        path: './build',
+        filename: 'app.bundle.js'
+    },
+
+    module: {
+        loaders: [{
+            test: /\.js$/,
+            exclude: /node_modules/,
+            loader: 'babel-loader'
+        }]
+    }
+};
+
